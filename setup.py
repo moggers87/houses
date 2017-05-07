78247f7690ee24a0f6f051ced83fdd66cd192c88
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-05-07 12:46:24 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="Houses",
+    description="A game of houses",
+    long_description="A game of houses",
+    author="Matt Molyneaux",
+    license="GPLv3+",
+    packages=['houses'],
+    test_suite="tests",
+)
---
+++ tests/houses_test.py
@@ -0,0 +1,6 @@
+from unittest import TestCase
+
+
+class TestingTestCase(TestCase):
+    def test_test(self):
+        self.assertEqual(1, 1)
